0baeb9f717355eac98c649c310df127638eb9a7a
Bintang <itzbintangyosua@gmail.com>
2024-11-08 10:06:43 +0700

    feat: add edit functionality for Mahasiswa
    Implement the ability to edit Mahasiswa entries by adding a modal for 
    editing and new methods to handle data retrieval and submission. 
    Introduce `openModalEdit` to open the edit modal and populate fields 
    with existing data, `editMahasiswa` to submit changes to the API, and 
    `ambilMahasiswa` to fetch data based on the selected entry's ID. 
    Enhance the UI to include an edit button within the Mahasiswa list.

--- src/app/mahasiswa/mahasiswa.page.ts
+++ src/app/mahasiswa/mahasiswa.page.ts
@@ -13,6 +13,7 @@ export class MahasiswaPage implements OnInit {
   id: any;
   nama: any;
   jurusan: any;
+  modalEdit: any;
 
   constructor(private api: ApiService, private modal: ModalController) {}
 
@@ -62,15 +63,53 @@ export class MahasiswaPage implements OnInit {
     });
   }
 
+  editMahasiswa() {
+    let data = {
+      id: this.id,
+      nama: this.nama,
+      jurusan: this.jurusan,
+    };
+    this.api.edit(data, 'edit.php').subscribe({
+      next: (hasil: any) => {
+        console.log(hasil);
+        this.resetModal();
+        this.getMahasiswa();
+        console.log('berhasil edit Mahasiswa');
+        this.modalEdit = false;
+        this.modal.dismiss();
+      },
+      error: (err: any) => {
+        console.log('gagal edit Mahasiswa');
+      },
+    });
+  }
+
+  ambilMahasiswa(id: any) {
+    this.api.lihat(id, 'lihat.php?id=').subscribe({
+      next: (hasil: any) => {
+        console.log('sukses', hasil);
+        let mahasiswa = hasil;
+        this.id = mahasiswa.id;
+        this.nama = mahasiswa.nama;
+        this.jurusan = mahasiswa.jurusan;
+      },
+      error: (error: any) => {
+        console.log('gagal ambil data');
+      },
+    });
+  }
+
   openModalTambah(isOpen: boolean) {
     this.modalTambah = isOpen;
     this.resetModal();
     this.modalTambah = true;
+    this.modalEdit = false;
   }
 
   cancel() {
     this.modal.dismiss();
     this.modalTambah = false;
+    this.modalEdit = false;
     this.resetModal();
   }
 
@@ -84,5 +123,14 @@ export class MahasiswaPage implements OnInit {
         console.log(err);
       },
     });
+  }
+
+  openModalEdit(isOpen: boolean, idget: any) {
+    this.modalEdit = isOpen;
+    this.id = idget;
+    console.log(this.id);
+    this.ambilMahasiswa(this.id);
+    this.modalTambah = false;
+    this.modalEdit = true;
   }
 }
