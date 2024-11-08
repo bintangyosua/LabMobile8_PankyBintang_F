02f7c7130bebeacb471161be2b8910d75198b08d
Bintang <itzbintangyosua@gmail.com>
2024-11-08 09:57:41 +0700

    first commit

---
+++ src/app/mahasiswa/mahasiswa.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mahasiswa',
+  templateUrl: './mahasiswa.page.html',
+  styleUrls: ['./mahasiswa.page.scss'],
+})
+export class MahasiswaPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
