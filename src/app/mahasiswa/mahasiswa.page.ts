dbabf9ac92e38e4eb390e39c9d6a98e10f17cad5
Bintang <itzbintangyosua@gmail.com>
2024-11-08 10:03:57 +0700

    feat(mahasiswa): enhance mahasiswa management UI and logic
    Update the mahasiswa page to improve data management with a new 
    header title and dynamic listing of mahasiswa entries. Integrate 
    modal functionality for adding new mahasiswa with form inputs for 
    name and course, ensuring better user interaction. Introduce 
    ApiService for handling HTTP requests to the backend, enabling full 
    CRUD operations for mahasiswa data.

--- src/app/mahasiswa/mahasiswa.page.ts
+++ src/app/mahasiswa/mahasiswa.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../service/api.service';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-mahasiswa',
@@ -6,10 +8,81 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mahasiswa.page.scss'],
 })
 export class MahasiswaPage implements OnInit {
+  dataMahasiswa: any;
+  modalTambah: any;
+  id: any;
+  nama: any;
+  jurusan: any;
 
-  constructor() { }
+  constructor(private api: ApiService, private modal: ModalController) {}
 
   ngOnInit() {
+    this.getMahasiswa();
   }
 
+  resetModal() {
+    this.id = null;
+    this.nama = '';
+    this.jurusan = '';
+  }
+
+  tambahMahasiswa() {
+    if (this.nama != '' && this.jurusan != '') {
+      let data = {
+        nama: this.nama,
+        jurusan: this.jurusan,
+      };
+      this.api.tambah(data, 'tambah.php').subscribe({
+        next: (hasil: any) => {
+          this.resetModal();
+          console.log('berhasil tambah mahasiswa');
+          this.getMahasiswa();
+          this.modalTambah = false;
+          this.modal.dismiss();
+        },
+        error: (err: any) => {
+          console.log('gagal tambah mahasiswa');
+        },
+      });
+    } else {
+      console.log('gagal tambah mahasiswa karena masih ada data yg kosong');
+    }
+  }
+
+  hapusMahasiswa(id: any) {
+    this.api.hapus(id, 'hapus.php?id=').subscribe({
+      next: (res: any) => {
+        console.log('sukses', res);
+        this.getMahasiswa();
+        console.log('berhasil hapus data');
+      },
+      error: (error: any) => {
+        console.log('gagal');
+      },
+    });
+  }
+
+  openModalTambah(isOpen: boolean) {
+    this.modalTambah = isOpen;
+    this.resetModal();
+    this.modalTambah = true;
+  }
+
+  cancel() {
+    this.modal.dismiss();
+    this.modalTambah = false;
+    this.resetModal();
+  }
+
+  getMahasiswa() {
+    this.api.tampil('tampil.php').subscribe({
+      next: (res: any) => {
+        console.log('sukses', res);
+        this.dataMahasiswa = res;
+      },
+      error: (err: any) => {
+        console.log(err);
+      },
+    });
+  }
 }
